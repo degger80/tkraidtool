04b272c6a7a6c1cc90e4b330d95fab281d973f12
Pavel <pavel.gorovoy@gmail.com>
2025-02-21 15:38:11 +0300

    additional info to discord message

--- server/methods/events/notifyEvent.js
+++ server/methods/events/notifyEvent.js
@@ -15,11 +15,12 @@ Meteor.methods({
       throw new Meteor.Error(403, 'Только админ или владелец инвента может удалить ивент');
     }
 
-    this.unblock();
+    // this.unblock();
 
-    Log(`/home/tkraidtool/.nvm/versions/node/v20.14.0/bin/node /home/tkraidtool/tk_raid_tool/discord/notify.js ${eventId} "${fmtDate(eventItem.startDateTime)} ${eventItem.title} ${eventItem.description} (${eventItem.ownerUsername}) ${eventItem.maxGroups * 5} мест"`)
-    console.log(123)
-    console.log(`/home/tkraidtool/.nvm/versions/node/v20.14.0/bin/node /home/tkraidtool/tk_raid_tool/discord/notify.js ${eventId} "${fmtDate(eventItem.startDateTime)} ${eventItem.title} ${eventItem.description} (${eventItem.ownerUsername}) ${eventItem.maxGroups * 5} мест"`)
+
+    Meteor.Log(`/home/tkraidtool/.nvm/versions/node/v20.14.0/bin/node /home/tkraidtool/tk_raid_tool/discord/notify.js ${eventId} "${fmtDate(eventItem.startDateTime)} ${eventItem.title} ${eventItem.description} (${eventItem.ownerUsername}) ${eventItem.maxGroups * 5} мест"`);
+    Meteor.Log(123);
+    console.log(`/home/tkraidtool/.nvm/versions/node/v20.14.0/bin/node /home/tkraidtool/tk_raid_tool/discord/notify.js ${eventId} "${fmtDate(eventItem.startDateTime)} ${eventItem.title} ${eventItem.description} (${eventItem.ownerUsername}) ${eventItem.maxGroups * 5} мест"`);
     // exec(`/home/tkraidtool/.nvm/versions/node/v20.14.0/bin/node /home/tkraidtool/tk_raid_tool/discord/notify.js ${eventId} "${fmtDate(eventItem.startDateTime)} ${eventItem.title} ${eventItem.description} (${eventItem.ownerUsername}) ${eventItem.maxGroups * 5} мест"`);
 
 
